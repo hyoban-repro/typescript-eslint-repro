5c5cbc5350d1c45e9dd349125161d8238543a230
Stephen Zhou <hi@hyoban.cc>
2024-02-14 18:07:56 +0800

    chore: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+import eslint from "@eslint/js"
+import tseslint from "typescript-eslint"
+import stylistic from "@stylistic/eslint-plugin"
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  stylistic.configs["recommended-flat"],
+)
